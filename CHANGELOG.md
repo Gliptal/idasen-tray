ec28e9747cfa988bc36fe9042145f3c885c66555
Mattia Affabris <mattia.affabris@antemotion.com>
2023-08-03 18:04:32 +0200

    change icon

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ### 0.1.0
 
 - First release.
+
+### 0.2.0
+
+- First properly functioning release.
--- idasen_tray/idasen_tray.py
+++ idasen_tray/idasen_tray.py
@@ -10,7 +10,7 @@ from infi.systray import SysTrayIcon
 import yaml
 
 
-__version__ = "0.3.0"
+__version__ = "0.2.0"
 
 logger = logging.getLogger("idasen-tray")
 
